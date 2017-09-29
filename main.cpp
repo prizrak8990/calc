6503d538511df65bd7cf659fb974810569fcc64b
prizrak8990 <32384718+prizrak8990@users.noreply.github.com>
2017-09-29 21:53:56 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+#include <stdio.h> 
+#include <stdlib.h> 
+using namespace std; 
+
+int main (int argc, char*argv[]) 
+{ 
+  char operand; 
+  operand = argv [2][0]; 
+  if ((isdigit (argv [1][0])) && (isdigit (argv [3][0]))) 
+    { 
+    switch (operand) 
+      { 
+      case '+': 
+      cout« atoi (argv[1]) + atoi (argv[3])« endl; 
+        break; 
+      case '-': 
+      cout« atoi (argv[1]) - atoi (argv[3])« endl; 
+        break; 
+          default: 
+      cerr« "error"« endl; 
+      }; 
+    } 
+  else { cerr« "ERROR"« endl;}; 
+}
